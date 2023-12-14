e5f6c8584211dd53ee1035ac9833151cdba443c0
WarZunx <yhuji.koled@gmail.com>
2023-12-15 00:55:38 +0100

    tirage carte + healthbar

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,5 +1,6 @@
 using JetBrains.Annotations;
 using System.Collections;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using static SaveAndLoad;
@@ -11,6 +12,9 @@ public class test : MonoBehaviour
     public PlayerList players = new PlayerList();
 
     public int tourCheckpoint = 0;
+    public int typeUse = 0;
+    public int texteUse = 0;
+    public string suiteTexte = "";
 
 
     [System.Serializable]
@@ -51,41 +55,80 @@ public class test : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            tirageCarte();
-            /*int test = Random.Range(0, players.player.Length);
-            int prout = Random.Range(0, players.player[0].texte[0].suite.Length);
-            Debug.Log(players.player[test].texte[0].suite.Length);
-            Debug.Log(players.player[test].typeCarte);
-            Debug.Log(players.player[test].texte[0].texteCarte);
-            Debug.Log(players.player[test].texte[0].suite[prout]);*/
-        }
-    }
-
-
-    public void tirageCarte()
-    {
-        int texteRange = Random.Range(0, players.player[0].texte[0].suite.Length);
-        int typeRange = Random.Range(0, players.player.Length-1);
-        //Debug.Log(players.player[typeRange].typeCarte);
-        //Debug.Log(players.player[typeRange].texte[0].texteCarte);
-        if (tourCheckpoint < 3)
-        {
-            if (players.player[typeRange].used == true)
+            if (suiteTexte != "")
             {
-                tirageCarte();
+                Debug.Log(suiteTexte);
+                suiteTexte = "";
             }
             else
             {
-                Debug.Log(players.player[typeRange].typeCarte);
-                Debug.Log(players.player[typeRange].texte[0].texteCarte);
-                players.player[typeRange].used = true;
-                tourCheckpoint++;
+                randomCarte();
+            }
+        }
+    }
+
+    public void randomCarte()
+    {
+        foreach (var item in players.player)
+        {
+            if (item.typeCarte != "checkpoint")
+            {
+                if (item.used == true)
+                {
+                    typeUse++;
+                    if (typeUse == players.player.Length - 1)
+                    {
+                        for (int i = 0; i < players.player.Length - 1; i++)
+                        {
+                            players.player[i].used = false;
+                        }
+                        tirageCarte();
+                        break;
+                    }
+                }
+                else
+                {
+                    typeUse = 0;
+                    tirageCarte();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void tirageCarte()
+    {
+        int typeRange = Random.Range(0, players.player.Length - 1);
+        int texteRange = Random.Range(0, players.player[typeRange].texte.Length);
+        int suiteRange = Random.Range(0, players.player[typeRange].texte[texteRange].suite.Length);
+
+        if (tourCheckpoint < 3)
+        {
+            if (players.player[typeRange].used == true)
+            {                
+                randomCarte();
+            }
+            else
+            {
+                if (players.player[typeRange].texte[texteRange].used == false)
+                {
+                    Debug.Log("Not Used");
+                    Debug.Log(players.player[typeRange].typeCarte);
+                    Debug.Log(players.player[typeRange].texte[texteRange].texteCarte);
+                    players.player[typeRange].used = true;
+                    players.player[typeRange].texte[texteRange].used = true;
+                    tourCheckpoint++;
+                    if (players.player[typeRange].texte[texteRange].suite.Length > 0)
+                    {                        
+                        suiteTexte = players.player[typeRange].texte[texteRange].suite[suiteRange];
+                    }
+                }
             }
         }
         else
         {
-            Debug.Log(players.player[3].typeCarte);
-            Debug.Log(players.player[3].texte[0].texteCarte);
+            Debug.Log(players.player[^1].typeCarte);
+            Debug.Log(players.player[^1].texte[0].texteCarte);
             tourCheckpoint = 0;
         }
     }
