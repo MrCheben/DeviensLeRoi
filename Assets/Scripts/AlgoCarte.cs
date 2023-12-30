90a85dc23f2f3578a6688d5f2222c19c193b5d5e
WarZunx <yhuji.koled@gmail.com>
2023-12-30 14:51:49 +0100

    NE PAS PRENDRE EN COMPTE !!!!

--- Assets/Scripts/AlgoCarte.cs
+++ Assets/Scripts/AlgoCarte.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 
 public class AlgoCarte : MonoBehaviour   
 {
-
+    public int NbCarteTirer;
     public TextAsset jsonData;
     public PlayerList players = new PlayerList();
     public GameObject btnChoix1;
@@ -68,6 +68,8 @@ public class AlgoCarte : MonoBehaviour
 
     public void tirageCarte()
     {
+        NbCarteTirer++;
+        Debug.Log("Nombre de carte tirer" + NbCarteTirer);
         Debug.Log("tirageCarte");
         if (!gameStarted)
         {
@@ -96,11 +98,9 @@ public class AlgoCarte : MonoBehaviour
 
     public void randomCarte()
     {
-
-
         foreach (var item in players.player)
         {
-            if (item.typeCarte != "checkpoint")
+            if (item.typeCarte != "Dîme")
             {
                 if (item.used == true)
                 {
@@ -129,9 +129,6 @@ public class AlgoCarte : MonoBehaviour
 
     public void checkCarte()
     {
-        
-
-
         int typeRange = Random.Range(0, players.player.Length - 1);
         int texteRange = Random.Range(0, players.player[typeRange].texte.Length);
         int suiteRange = Random.Range(0, players.player[typeRange].texte[texteRange].suite.Length);
